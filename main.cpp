6868491a07be85d5d858b41e3c933c1cdff43c3c
SUPERETDUPER <machst@live.ca>
2019-01-18 13:50:00 -0500

    Fix problems with dependencies
    Signed-off-by: SUPERETDUPER <machst@live.ca>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 #include "lib/demo.h"
+//#include <SoftwareSerial.h>
 
 void setup() {
     Serial.begin(9600);
     Serial.println("Running main");
     test();
+    //SoftwareSerial* softwareSerial = new SoftwareSerial(3,4);
 }
 
 void loop() {
